d70ba07b9d32691efd3b4a3af943ae5e3327289b
JIm21d <Jim123asd5@proton.me>
2023-09-20 16:00:30 -0400

    lab 2 test

---
+++ Lab_2/src/model/VitalSigns.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 15469
+ */
+public class VitalSigns {
+    private double temperature;
+    private double bloodPressure;
+    private int pulse;
+    private String date;
+    
+}
