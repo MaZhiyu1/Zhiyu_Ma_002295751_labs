4e544bf83e0f2eb6f07175edb695d91374e8bf34
JIm21d <ma.zhiyu1@northeastern.edu>
2023-11-07 21:55:59 -0500

    Lab 5

---
+++ Lab_5/src/model/OrderItem.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 15469
+ */
+public class OrderItem {
+    Product product;
+    Double salesPrice;
+    int quantity;
+
+    public OrderItem(Product product, Double salesPrice, int quantity) {
+        this.product = product;
+        this.salesPrice = salesPrice;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(Double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public  String toString(){
+        return this.product.toString();
+    }
+    
+}
